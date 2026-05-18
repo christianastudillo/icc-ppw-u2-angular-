8e3b086e650fd3bf4522cc2683babc669f108222
christianastudillo <chris.astu134@gmail.com>
2026-05-18 18:06:44 -0500

    Fin 04

--- src/app/features/layouts/pages/layouts-page.ts
+++ src/app/features/layouts/pages/layouts-page.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layouts-page',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './layouts-page.html',
 })
-export class LayoutsPage {}
+export class LayoutsPage {
+
+}
