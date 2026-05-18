36f26a020ed634452bf32b71c925364b4c7a7fdc
christianastudillo <chris.astu134@gmail.com>
2026-05-18 04:22:54 -0500

    AngularIns

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomePage } from './features/home/pages/home-page/home-page';
 import { StudentsPage } from './features/students/pages/students-page/students-page';
 import { StudentDetailPage } from './features/students/pages/student-detail-page/student-detail-page';
+import { LayoutsPage } from './features/layouts/pages/layouts-page';
 
 export const routes: Routes = [
     { path: '', component: HomePage },
     { path: 'students', component: StudentsPage },
     { path: 'students/:id', component: StudentDetailPage },
-    { path: '**', redirectTo: '' }
+    { path: '**', redirectTo: '' },
+    { path: 'layouts', component: LayoutsPage}
 ];
---
+++ src/app/features/layouts/pages/layouts-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layouts-page',
+  standalone: true,
+  templateUrl: './layouts-page.html',
+})
+export class LayoutsPage {}
